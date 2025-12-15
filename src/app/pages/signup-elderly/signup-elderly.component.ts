9f9b789f1e66f929a729a8ed19b197018c4d62b1
stijnpotters <stijn.potters1@gmail.com>
2025-12-15 22:08:58 +0100

    added elderly signup page and about

--- src/app/pages/signup-elderly/signup-elderly.component.ts
+++ src/app/pages/signup-elderly/signup-elderly.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup-elderly',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signup-elderly.component.html',
   styleUrl: './signup-elderly.component.scss'
 })
 export class SignupElderlyComponent {
+  currentStep = 1;
+
   formData = {
     name: '',
-    email: '',
     age: '',
+    city: '',
+    nursingHome: '',
+    email: '',
     interests: ''
   };
 
   constructor(private router: Router) {}
+
+  goToNextStep() {
+    this.currentStep = 2;
+  }
+
+  goToPreviousStep() {
+    this.currentStep = 1;
+  }
 
   onSubmit() {
     this.router.navigate(['/contact-soon']);
