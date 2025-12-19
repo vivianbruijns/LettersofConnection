af99101d3504acb3a1bc404045a34627340a6ec8
stijnpotters <stijn.potters1@gmail.com>
2025-12-19 17:06:57 +0100

    code cleanup

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
