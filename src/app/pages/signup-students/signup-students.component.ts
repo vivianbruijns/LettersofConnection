f44caa559cd8096c4b4e69cf2a0b49bd421ccf24
stijnpotters <stijn.potters1@gmail.com>
2025-12-21 16:44:42 +0100

    finished step 1 student signup

--- src/app/pages/signup-students/signup-students.component.ts
+++ src/app/pages/signup-students/signup-students.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup-students',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signup-students.component.html',
   styleUrl: './signup-students.component.scss'
 })
 export class SignupStudentsComponent {
+  currentStep = 1;
+
   formData = {
     name: '',
-    email: '',
     age: '',
+    grade: '',
     school: '',
-    interests: ''
+    city: '',
+    interests: '',
+    hobby: '',
+    favoriteSubject: '',
+    contact: ''
   };
 
   constructor(private router: Router) {}
+
+  goToNextStep() {
+    this.currentStep = 2;
+  }
+
+  goToPreviousStep() {
+    this.currentStep = 1;
+  }
 
   onSubmit() {
     this.router.navigate(['/contact-soon']);
